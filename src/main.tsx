93bc598e7dce1442371fdf175df2f9b2743da064
giovanagarcia1609-cell <giovana.garcia1609@gmail.com>
2026-02-24 17:04:15 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,9 @@
-Permission denied
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
